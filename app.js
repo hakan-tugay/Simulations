e12b34fd742ab3a9d4f020129efb1023eba272c1
Hakan Tugay <f3tugay@gmail.com>
2017-12-23 15:47:37 +0200

    Calculate integrals

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var program = require('commander');
+var rndm = require('random-number');
+var _ = require('lodash');
+
+
 program
   .version('1.0.0')
   .description('Simulations');
@@ -23,11 +27,48 @@ program
 program.parse(process.argv);
 
 function integralSimulation(){
-    console.log(111)
+    const result = integrator(0,1000,sourceFunction4)
+    console.log('Integral', result)
 }
 
 function integrator(x1, x2, sourceFunction){
+    const y1 = sourceFunction(x1)
+    const y2 = sourceFunction(x2)
 
+    console.log('Y values:', y1, y2)
+    const totalRectangleArea = Math.max(y1, y2) * Math.abs(x1 - x2) 
+    const xGenerator = rndm.generator({
+      min: x1,
+      max:  x2,
+      integer: false
+    })
+
+    const yGenerator = rndm.generator({
+      min: 0,
+      max:  Math.max(y1, y2),
+      integer: false
+    })
+
+    let hits = 0
+
+    const sampleSize = 10000000
+    _.times(sampleSize, n=>{
+      const randomX = xGenerator()
+      const randomY = yGenerator()
+      //console.log(randomX, randomY)
+
+      const yOfRandomX = sourceFunction(randomX)
+
+      if(randomY < yOfRandomX){
+        hits++
+        //console.log(randomX, randomY, yOfRandomX, 'Hit')
+      }  
+    })
+    
+    let hitRatio = hits / sampleSize;
+    console.log('Hits', hitRatio, totalRectangleArea)
+    let integralArea = totalRectangleArea * hitRatio
+    return integralArea
 }
 
 function sourceFunction1(x){
@@ -35,6 +76,14 @@ function sourceFunction1(x){
 }
 
 function sourceFunction2(x){
-    return 2*x*x+6*x-8;
+    return 2*x*x+6*x+8;
+}
+
+function sourceFunction3(x){
+  return Math.pow(2, x)
+}
+
+function sourceFunction4(x){
+  return Math.pow(1/2, x)
 }
 
